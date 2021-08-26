cef343497d2eb25095395731bf50741123e78bba
fares anes <itsfares11@gmail.com>
2021-08-26 14:37:28 +0100

    final version

--- src/app/pages/collaboration/collaboration.component.ts
+++ src/app/pages/collaboration/collaboration.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { ArchiveService } from 'app/services/archive.service';
 import { Company } from 'app/services/company';
+import { Container } from 'app/services/container';
 import { ContainerService } from 'app/services/container.service';
 import { UserInfoService } from 'app/services/user-info.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-collaboration',
@@ -14,38 +16,24 @@ export class CollaborationComponent implements OnInit {
   archive
   name:any
   p:number=1
+  contianerid
+  companyid
+  public dataRows :Array<Observable<Container>>
 
   constructor(public container:ContainerService, public archive1:ArchiveService, public userinfo:UserInfoService, public db:AngularFireDatabase ) { }
 
 
 
   ngOnInit(): void {
-    this.container
+    this.dataRows=this.container.getcoll()
+
+
 
   }
-  key='id';
-  reverse:boolean=false;
-  sort(key){
-    this.key=key
-    this.reverse= !this.reverse
+  addColl(){
+    this.container.setupcoll(this.contianerid,this.companyid)
+  }
 
-  }
-  sreach(){
-    if(this.name==""){
-      let user =this.userinfo.getUserInfo().subscribe(user=>{
-        this.db.object("companies/"+user.companyId).valueChanges().subscribe((res:Company)=>{
-          console.log(res)
-          this.archive=res.archive
-          this.archive.shift()
-          this.archive.shift()
-          console.log( this.archive) 
-        })
-      })
-    }else{
-      this.archive= this.archive.filter(res=>{
-        return res.name.toLocaleLowerCase().match(this.name.toLocaleLowerCase())
-      })
-    }
-  }
+
 
 }
